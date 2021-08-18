cc5bf9ff8086b58537e0813415e24e62dcb318df
Osama Altawil <oaltawil@microsoft.com>
2021-08-18 18:05:39 -0400

    Modified location of downloads for zip files.

--- Configure-AzureVMImageBuilder.ps1
+++ Configure-AzureVMImageBuilder.ps1
@@ -26,7 +26,7 @@ $location="eastus"
 $runOutputName="aibCustomWindows7Image"
 
 # Image template name
-$imageTemplateName="aibCustomWindows7ImageTemplateVer2"
+$imageTemplateName="aibCustomWindows7ImageTemplateVer4"
 
 # The Image Template File Name
 $templateFilePath = "armTemplateWinSIG_New.json"
