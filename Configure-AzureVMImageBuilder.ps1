214e85faf1c5ec35e9042c132f951e7367be736e
Osama Altawil <oaltawil@microsoft.com>
2021-08-18 21:12:16 -0400

    Moved Install-WMF & all scripts run elevated.

--- Configure-AzureVMImageBuilder.ps1
+++ Configure-AzureVMImageBuilder.ps1
@@ -26,7 +26,7 @@ $location="eastus"
 $runOutputName="aibCustomWindows7Image"
 
 # Image template name
-$imageTemplateName="aibCustomWindows7ImageTemplateVer5"
+$imageTemplateName="aibCustomWindows7ImageTemplateVer6"
 
 # The Image Template File Name
 $templateFilePath = "armTemplateWinSIG_New.json"
