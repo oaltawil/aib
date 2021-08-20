77216203e35b9245cf71265b9e1f766301289978
Osama Altawil <oaltawil@microsoft.com>
2021-08-19 20:55:46 -0400

    no luck.

--- Configure-AzureVMImageBuilder.ps1
+++ Configure-AzureVMImageBuilder.ps1
@@ -1,15 +1,3 @@
-Register-AzResourceProvider -ProviderNamespace Microsoft.VirtualMachineImages
-Register-AzResourceProvider -ProviderNamespace Microsoft.Storage
-Register-AzResourceProvider -ProviderNamespace Microsoft.Compute
-Register-AzResourceProvider -ProviderNamespace Microsoft.KeyVault
-Register-AzResourceProvider -ProviderNamespace Microsoft.Network
-
-Get-AzResourceProvider -ProviderNamespace Microsoft.VirtualMachineImages | Format-table -Property ResourceTypes,RegistrationState
-Get-AzResourceProvider -ProviderNamespace Microsoft.Storage | Format-table -Property ResourceTypes,RegistrationState 
-Get-AzResourceProvider -ProviderNamespace Microsoft.Compute | Format-table -Property ResourceTypes,RegistrationState
-Get-AzResourceProvider -ProviderNamespace Microsoft.KeyVault | Format-table -Property ResourceTypes,RegistrationState
-Get-AzResourceProvider -ProviderNamespace Microsoft.Network | Format-table -Property ResourceTypes,RegistrationState
-
 # Get existing context
 $currentAzContext = Get-AzContext
 
@@ -26,7 +14,7 @@ $location="eastus"
 $runOutputName="aibCustomWindows7Image"
 
 # Image template name
-$imageTemplateName="aibCustomWindows7ImageTemplateVer6"
+$imageTemplateName="aibCustomWindows7ImageTemplateVer17"
 
 # The Image Template File Name
 $templateFilePath = "armTemplateWinSIG_New.json"
@@ -40,88 +28,11 @@ $imageDefName ="customWindows7Image"
 # additional replication region
 $replRegion2="eastus2"
 
-
-# Create a resource group for Image Template and Shared Image Gallery
-New-AzResourceGroup `
-   -Name $imageResourceGroup `
-   -Location $location
-
-# setup role def names, these need to be unique
-$timeInt=$(get-date -UFormat "%s")
-$imageRoleDefName="Azure Image Builder Image Def"+$timeInt
-$identityName="aibIdentity"+$timeInt
-
-## Add AZ PS module to support AzUserAssignedIdentity
-Install-Module -Name Az.ManagedServiceIdentity
-
-# create identity
-New-AzUserAssignedIdentity -ResourceGroupName $imageResourceGroup -Name $identityName
-
-$identityNameResourceId=$(Get-AzUserAssignedIdentity -ResourceGroupName $imageResourceGroup -Name $identityName).Id
-$identityNamePrincipalId=$(Get-AzUserAssignedIdentity -ResourceGroupName $imageResourceGroup -Name $identityName).PrincipalId
-
-$aibRoleImageCreationUrl="https://raw.githubusercontent.com/azure/azvmimagebuilder/master/solutions/12_Creating_AIB_Security_Roles/aibRoleImageCreation.json"
-$aibRoleImageCreationPath = "aibRoleImageCreation.json"
-
-# download config
-Invoke-WebRequest -Uri $aibRoleImageCreationUrl -OutFile $aibRoleImageCreationPath -UseBasicParsing
-
-((Get-Content -path $aibRoleImageCreationPath -Raw) -replace '<subscriptionID>',$subscriptionID) | Set-Content -Path $aibRoleImageCreationPath
-((Get-Content -path $aibRoleImageCreationPath -Raw) -replace '<rgName>', $imageResourceGroup) | Set-Content -Path $aibRoleImageCreationPath
-((Get-Content -path $aibRoleImageCreationPath -Raw) -replace 'Azure Image Builder Service Image Creation Role', $imageRoleDefName) | Set-Content -Path $aibRoleImageCreationPath
-
-# create role definition
-New-AzRoleDefinition -InputFile  ./aibRoleImageCreation.json
-
-# grant role definition to image builder service principal
-New-AzRoleAssignment -ObjectId $identityNamePrincipalId -RoleDefinitionName $imageRoleDefName -Scope "/subscriptions/$subscriptionID/resourceGroups/$imageResourceGroup"
-
-# Create the gallery
-New-AzGallery `
-   -GalleryName $sigGalleryName `
-   -ResourceGroupName $imageResourceGroup  `
-   -Location $location
-
-# Create the image definition
-New-AzGalleryImageDefinition `
-   -GalleryName $sigGalleryName `
-   -ResourceGroupName $imageResourceGroup `
-   -Location $location `
-   -Name $imageDefName `
-   -OsState Generalized `
-   -OsType Windows `
-   -Publisher 'ScrapaperCorporation' `
-   -Offer 'windows-7' `
-   -Sku 'enterprise'
-
-Invoke-WebRequest `
-   -Uri "https://raw.githubusercontent.com/azure/azvmimagebuilder/master/quickquickstarts/1_Creating_a_Custom_Win_Shared_Image_Gallery_Image/armTemplateWinSIG.json" `
-   -OutFile $templateFilePath `
-   -UseBasicParsing
-
-(Get-Content -path $templateFilePath -Raw ) `
-   -replace '<subscriptionID>',$subscriptionID | Set-Content -Path $templateFilePath
-(Get-Content -path $templateFilePath -Raw ) `
-   -replace '<rgName>',$imageResourceGroup | Set-Content -Path $templateFilePath
-(Get-Content -path $templateFilePath -Raw ) `
-   -replace '<runOutputName>',$runOutputName | Set-Content -Path $templateFilePath
-(Get-Content -path $templateFilePath -Raw ) `
-   -replace '<imageDefName>',$imageDefName | Set-Content -Path $templateFilePath
-(Get-Content -path $templateFilePath -Raw ) `
-   -replace '<sharedImageGalName>',$sigGalleryName | Set-Content -Path $templateFilePath
-(Get-Content -path $templateFilePath -Raw ) `
-   -replace '<region1>',$location | Set-Content -Path $templateFilePath
-(Get-Content -path $templateFilePath -Raw ) `
-   -replace '<region2>',$replRegion2 | Set-Content -Path $templateFilePath
-((Get-Content -path $templateFilePath -Raw) -replace '<imgBuilderId>',$identityNameResourceId) | Set-Content -Path $templateFilePath\
-
-
 New-AzResourceGroupDeployment `
 -ResourceGroupName $imageResourceGroup `
 -TemplateFile $templateFilePath `
--api-version "2020-02-14" `
 -imageTemplateName $imageTemplateName `
--svclocation $location
+-location $location
 
 Invoke-AzResourceAction `
 -ResourceName $imageTemplateName `
@@ -130,20 +41,3 @@ Invoke-AzResourceAction `
 -ApiVersion "2020-02-14" `
 -Action Run `
 -Force
-
-
-$isRunning = $?
-
-while ($isRunning) {
-
-   $AzImageBuilderTemplate = Get-AzImageBuilderTemplate -ImageTemplateName $imageTemplateName -ResourceGroupName $imageResourceGroup
-   
-   $isRunning = $AzImageBuilderTemplate.LastRunStatusRunState -eq "Running"
-
-   $AzImageBuilderTemplate | Select-Object -Property Name, LastRunStatusRunState, LastRunStatusMessage, ProvisioningState
-
-   Clear-Host
-   
-   Start-Sleep -Seconds 15
-
-}
